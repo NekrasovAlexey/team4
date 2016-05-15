5badfa9489f66ee33eed11f7cf3f1d54d728e0da
NekrasovAlexey <creepy404nf@gmail.com>
2016-05-15 15:57:04 +0500

    Update place.js

--- blocks/place/place.js
+++ blocks/place/place.js
@@ -1,6 +1,27 @@
 $('.check-in').each(function () {
     $(this).click(function () {
-        var name = $(this).data('name');
-        console.log(name);// quest#place
+        var names = $(this).data('name').split('#');
+        var questName = names[0];
+        var placeName = names[1];
+        var options = {
+            enableHighAccuracy: true,
+            maximumAge: 50000,
+            timeout: 10000
+        };
+        navigator.geolocation.getCurrentPosition(
+
+            function (position) {
+                $.ajax({
+                    url: '/quest/checkin/',
+                    type: 'POST',
+                    data: {
+                        quest: questName,
+                        place: placeName,
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude
+                    }
+                });
+            }
+        );
     });
 });
